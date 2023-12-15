c8b0e43705aea4f71943a5432a8a1029e09eeb38
tastaw23 <younesmekki2@gmail.com>
2023-12-14 19:18:05 -0800

    monty

--- monty.h
+++ monty.h
@@ -72,13 +72,3 @@ void mod_nodes(stack_t **, unsigned int);
 
 /*String operations*/
 void print_char(stack_t **, unsigned int);
-void print_str(stack_t **, unsigned int);
-void rotl(stack_t **, unsigned int);
-
-/*Error hanlding*/
-void err(int error_code, ...);
-void more_err(int error_code, ...);
-void string_err(int error_code, ...);
-void rotr(stack_t **, unsigned int);
-
-#endif
